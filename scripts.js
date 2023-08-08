089cd397f8eb832964090f6da13d9fa0e24aa0fd
Pablo Entony <pabloentony331@gmail.com>
2023-08-08 20:27:28 -0300

    Alterada 27/07/2023 20:47

---
+++ scripts.js
@@ -0,0 +1,14 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const form = document.getElementById("contact-form");
+
+    form.addEventListener("submit", (event) => {
+        event.preventDefault();
+
+        const name = form.elements["nome"].value;
+        const email = form.elements["email"].value;
+        const message = form.elements["mensagem"].value;
+        const result = `Nome: ${name}\n E-mail: ${email}\n Mensagem: ${message}`;
+        
+        alert(result);
+    });
+});
